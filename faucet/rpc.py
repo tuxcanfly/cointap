c91145f2729b2e063a738aabf684ecb837296f9a
Javed Khan <tuxcanfly@gmail.com>
2018-09-20 11:14:40 +0530

    rpc: show unconfirmed balance

--- faucet/rpc.py
+++ faucet/rpc.py
@@ -9,7 +9,7 @@ class WalletClient:
         url = '%s/wallet/%s/balance' % (settings.COIN_NODE, settings.COIN_WALLET)
         response = requests.get(url, auth=settings.COIN_AUTH)
         json = response.json()
-        return json['confirmed']
+        return json['unconfirmed']
 
     def send(address, amount):
         url = '%s/wallet/%s/send' % (settings.COIN_NODE, settings.COIN_WALLET)
